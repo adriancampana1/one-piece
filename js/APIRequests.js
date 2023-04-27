cd10546eca5ed41952e7c1c095ff734546f1cfab
Adrian Campana <adrianphcampana12@gmail.com>
2023-04-26 22:47:02 -0300

    js

--- js/APIRequests.js
+++ js/APIRequests.js
@@ -1,9 +1,18 @@
+// recebe os dados de todas as akuma no mi
 const API_AKUMA_NO_MI = 'https://api.api-onepiece.com/fruits';
+
+// recebe os dados de todas as espadas
 const API_SWORDS = 'https://api.api-onepiece.com/swords';
+// recebe os dados de todos os tipos de haki
 const API_HAKI = 'https://api.api-onepiece.com/hakis';
+
+// recebe os dados de todas as gears do luffy
 const API_GEARS_LUFFY = 'https://api.api-onepiece.com/luffy/gears';
+// recebe os dados de todas as técnicas do luffy
 const API_LUFFY_TECHNIQUES = 'https://api.api-onepiece.com/luffy/gears';
 
+// lista TODOS os personagens de one piece
+const API_ALL_CHARACTERS = 'https://api.api-onepiece.com/characters';
 getMovies(API_AKUMA_NO_MI);
 getMovies(API_GEARS_LUFFY);
 
